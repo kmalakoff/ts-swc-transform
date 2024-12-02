08970ce558fe3743558db465536a40db4c999599
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-01 16:43:49 -0800

    Tweak

--- src/transformFile.mjs
+++ src/transformFile.mjs
@@ -98,7 +98,7 @@ function transformFileCallback(src, dest, type, options, callback) {
  * @param {string} dest The output directory to write the file to.
  * @param {string} type The type of transform ('esm' or 'cjs').
  * @param {{sourceMaps: boolean}} options Options to pass to swc.
- * @param {(err?: Error, destFilePath: string) =>} [callback] Optional callback returing the path to the transformed file. Uses promise if callback not provided.
+ * @param {(err: Error | null, destFilePath: string) =>} [callback] Optional callback returing the path to the transformed file. Uses promise if callback not provided.
  * @returns {void | Promise<string>} Optional promise returing the path to the transformed file if callback not provided.
  */
 export default function transformFile(src, dest, type, options, callback) {
