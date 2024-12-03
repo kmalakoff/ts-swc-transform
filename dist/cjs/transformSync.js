aa138c7a66a15ff7716b69ff8d2a28f6d426be79
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 21:49:37 -0800

    Upgrade modules

--- dist/cjs/transformSync.js
+++ dist/cjs/transformSync.js
@@ -12,4 +12,4 @@ var call = null; // break dependencies
     if (!call) call = require('node-version-call'); // break dependencies
     return call(version, worker, contents, fileName, config);
 };
-/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; }; module.exports = exports.default; } catch (_) {} }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }
