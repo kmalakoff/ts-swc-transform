33939e09bccd98a46e7d369f5da0feb46abdd21c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 19:01:23 -0800

    Upgrade modules

--- dist/esm/createMatcher.mjs
+++ dist/esm/createMatcher.mjs
@@ -1,13 +1,13 @@
 import minimatch from 'minimatch';
 import path from 'path-posix';
-import slash from 'slash';
+import unixify from 'unixify';
 /**
  * @param {{path: string, config: Object}} config The path to the loaded TS config and typescript config.
  * @returns {(filePath:string) => boolean} The function to test for typescript files being included or excluded
  */ export default function createMatcher(config) {
-    const configPath = path.dirname(slash(config.path));
+    const configPath = path.dirname(unixify(config.path));
     function matchFn(condition) {
-        let pattern = slash(condition);
+        let pattern = unixify(condition);
         if (!path.isAbsolute(pattern) && !pattern.startsWith('*')) pattern = path.join(configPath, pattern);
         return function match(filePath) {
             return filePath.startsWith(pattern) || minimatch(filePath, pattern);
@@ -17,7 +17,7 @@ import slash from 'slash';
     const excludes = (config.config.exclude || []).map(matchFn);
     return function matcher(filePath) {
         if (filePath.endsWith('.json')) return false;
-        filePath = slash(filePath);
+        filePath = unixify(filePath);
         for(let i = 0; i < excludes.length; ++i){
             if (excludes[i](filePath)) return false;
         }
