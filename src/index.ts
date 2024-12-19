00d767fec4af9c182fe7033359e8ab6720224172
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-18 20:19:10 -0800

    Exported toPath

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './types.js';
 export { default as createMatcher } from './createMatcher.js';
 export { default as extensions } from './extensions.js';
 export { default as resolveFileSync } from './resolveFileSync.js';
+export { default as toPath } from './toPath.js';
 export { default as transformFile } from './transformFile.js';
 export { default as transformDirectory } from './transformDirectory.js';
 export { default as transformSync } from './transformSync.js';
