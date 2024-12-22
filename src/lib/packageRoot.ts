e9e48f12582977b498078b1370c28480245f0fef
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 16:50:12 -0800

    Upgrade modules

--- src/lib/packageRoot.ts
+++ src/lib/packageRoot.ts
@@ -1,8 +1,11 @@
+import fs from 'fs';
 import path from 'path';
+import existsSync from './existsSync.js';
 
-export default function packageRoot(dir, packageName) {
-  if (path.basename(dir) === packageName) return dir;
+export default function packageRoot(dir) {
+  const packagePath = path.join(dir, 'package.json');
+  if (existsSync(packagePath) && JSON.parse(fs.readFileSync(packagePath, 'utf8')).name) return dir;
   const nextDir = path.dirname(dir);
-  if (nextDir === dir) throw new Error(`${packageName} not found`);
-  return packageRoot(nextDir, packageName);
+  if (nextDir === dir) throw new Error('Package root not found');
+  return packageRoot(nextDir);
 }
