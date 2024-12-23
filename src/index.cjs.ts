3ac4a60bfb5d972a6131530e837b214ffcde467a
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 23:36:13 -0800

    Upgrade modules

--- src/index.cjs.ts
+++ src/index.cjs.ts
@@ -1,8 +1,4 @@
-export type * from './types.js';
-export { default as createMatcher } from './createMatcher.js';
-export { default as extensions } from './extensions.js';
-export { default as resolveFileSync } from './resolveFileSync.js';
-export { default as toPath } from './toPath.js';
+export * from './index-base.js';
 export { default as transformFile } from './cjs/transformFile.js';
 export { default as transformDirectory } from './cjs/transformDirectory.js';
 export { default as transformSync } from './cjs/transformSync.js';
