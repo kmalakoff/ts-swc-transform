121a41f3e53390f26c6520393d01cd9b4c443589
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 20:49:42 -0800

    Upgrade modules

--- src/workers/transformDirectory.ts
+++ src/workers/transformDirectory.ts
@@ -1,23 +1,32 @@
 import path from 'path';
 import Iterator from 'fs-iterator';
 import getTS from 'get-tsconfig-compat';
+import Queue from 'queue-cb';
 
+import { typeFileRegEx } from '../constants.js';
 import createMatcher from '../createMatcher.js';
-import transformFile from '../transformFile.js';
+import transformFile from './transformFile.js';
 
 export default function transformDirectoryWorker(src, dest, type, options, callback) {
   const tsconfig = options.tsconfig ? options.tsconfig : getTS.getTsconfig(src);
   const matcher = createMatcher(tsconfig);
 
-  options = { ...options, tsconfig };
+  const entries = [];
   const iterator = new Iterator(src);
   iterator.forEach(
-    (entry, cb) => {
-      if (!entry.stats.isFile()) return cb();
-      if (!matcher(entry.fullPath)) return cb();
-      transformFile(entry.fullPath, path.dirname(path.join(dest, entry.path)), type, options, cb);
+    (entry) => {
+      if (!entry.stats.isFile()) return;
+      if (!matcher(entry.fullPath)) return;
+      if (typeFileRegEx.test(entry.basename)) return;
+      entries.push(entry);
     },
-    { callbacks: true, concurrency: options.concurrency || 1024 },
-    callback
+    (err) => {
+      if (err) return callback(err);
+
+      options = { ...options, tsconfig };
+      const queue = new Queue();
+      entries.forEach((entry) => queue.defer(transformFile.bind(null, entry.fullPath, path.dirname(path.join(dest, entry.path)), type, options)));
+      queue.await(callback);
+    }
   );
 }
