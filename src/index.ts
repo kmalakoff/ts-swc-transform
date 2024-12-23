6a68e2b13b1506174fc3d46f34a6a5237093d036
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 09:47:44 -0800

    Simlified workers

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export * as constants from './constants.js';
 export { default as extensions } from './extensions.js';
 export { default as resolveFileSync } from './resolveFileSync.js';
 export { default as toPath } from './toPath.js';
+
+export { default as transformFile } from './transformFile.js';
+export { default as transformDirectory } from './transformDirectory.js';
+export { default as transformSync } from './transformSync.js';
+export { default as transformTypes } from './transformTypes.js';
