233e8d9dc5e4739e931284eb50867ff9feb9aaed
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 23:36:13 -0800

    Upgrade modules

--- src/index-base.ts
+++ src/index-base.ts
@@ -4,6 +4,3 @@ export * as constants from './constants.js';
 export { default as extensions } from './extensions.js';
 export { default as resolveFileSync } from './resolveFileSync.js';
 export { default as toPath } from './toPath.js';
-export { default as transformFile } from './esm/transformFile.js';
-export { default as transformDirectory } from './esm/transformDirectory.js';
-export { default as transformSync } from './esm/transformSync.js';
