980a317c65cb2a92a97ea5ac68677184abc60dd2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 10:10:45 -0800

    Upgrade modules

---
+++ test/unit/transformSync.test.ts
@@ -0,0 +1,27 @@
+// remove NODE_OPTIONS from ts-dev-stack
+// biome-ignore lint/performance/noDelete: <explanation>
+delete process.env.NODE_OPTIONS;
+
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import getTS from 'get-tsconfig-compat';
+
+import assert from 'assert';
+
+// @ts-ignore
+import { transformSync } from 'ts-swc-transform';
+
+const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+const SRC_DIR = path.resolve(__dirname, '..', 'data', 'src');
+const tsconfig = getTS.getTsconfig(SRC_DIR);
+
+describe('transformSync', () => {
+  it('test.ts', () => {
+    const filePath = path.join(SRC_DIR, 'test.ts');
+    const contents = fs.readFileSync(filePath, 'utf8');
+    const res = transformSync(contents, filePath, tsconfig);
+    assert.ok(!!res.code);
+    assert.equal(typeof res.code, 'string');
+  });
+});
