eb20db425aa402f9996808f805bb2cf46a54a96d
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 23:28:40 -0800

    Upgrade modules

--- src/lib/installBindings.ts
+++ src/lib/installBindings.ts
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import installModule from 'install-module-linked';
 import Queue from 'queue-cb';
 import resolve from 'resolve';
-import installModule from './install-module/index.js';
+
 export default function installBindings(callback) {
   const swcPackagePath = resolve.sync('@swc/core/package.json');
   const swcDir = path.dirname(path.dirname(swcPackagePath));
   const { optionalDependencies } = JSON.parse(fs.readFileSync(swcPackagePath, 'utf8'));
-  const _installDir = path.dirname(swcDir);
   const depKey = `${process.platform}-${process.arch}`;
 
   const queue = new Queue();
@@ -17,8 +17,7 @@ export default function installBindings(callback) {
       queue.defer((callback) => {
         const version = optionalDependencies[name];
         const installString = `${name}${version ? `@${version}` : ''}`;
-        const installedAt = path.join(path.dirname(swcDir), ...name.split('/'));
-        installModule(installString, installedAt, callback);
+        installModule(installString, path.dirname(swcDir), callback);
       });
     });
   queue.await(callback);
