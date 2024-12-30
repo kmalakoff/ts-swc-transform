870a246e553978f44674935892fb41e950d821cb
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-30 02:15:34 -0800

    Implement loadTsConfig to automatically resolve tsconfig files

--- src/transformSync.ts
+++ src/transformSync.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import url from 'url';
+import * as getTS from 'get-tsconfig-compat';
+
 import wrapWorkerSync from './lib/wrapWorkerSync.js';
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
 const workerWrapper = wrapWorkerSync(path.resolve(__dirname, '..', 'cjs', 'workers', 'transformSync.js'));
@@ -13,5 +15,8 @@ import type { TsConfigResult } from 'get-tsconfig-compat';
  * @returns {{ code: string, map?: string }} Returns object with the transformed code and source map if option sourceMaps was provided.
  */
 export default function transformSync(contents: string, fileName: string, tsconfig: TsConfigResult): Output {
+  if (typeof contents !== 'string') throw new Error('transformTypes: unexpected contents');
+  if (typeof fileName !== 'string') throw new Error('transformTypes: unexpected fileName');
+  if (!tsconfig) tsconfig = getTS.getTsconfig(process.cwd());
   return workerWrapper('stable', contents, fileName, tsconfig);
 }
