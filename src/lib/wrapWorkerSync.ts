7b2f5debc8f6468d0221a69180337b7bf9f6b4f6
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 20:04:28 -0800

    Upgrade modules

--- src/lib/wrapWorkerSync.ts
+++ src/lib/wrapWorkerSync.ts
@@ -1,10 +1,13 @@
-// @ts-ignore
-import lazy from './lazy.cjs';
-const call = lazy('node-version-call');
+import Module from 'module';
+import lazy from 'lazy-cache';
+const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
+const call = lazy(_require)('node-version-call');
 
 export default function wrapWorkerSync(workerPath: string) {
+  const workerLazy = lazy(_require)(workerPath);
+
   return function workerWrapperSync(version, ...args) {
-    if (version === 'local') return lazy(workerPath)().apply(null, args);
+    if (version === 'local') return workerLazy().apply(null, args);
     return call()({ version, callbacks: true }, workerPath, ...args);
   };
 }
