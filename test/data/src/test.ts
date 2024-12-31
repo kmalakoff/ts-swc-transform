59f764919b3328c88eb563c56bb175601dd0abde
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-31 12:00:51 -0800

    Upgrade typescript to infer extensions

--- test/data/src/test.ts
+++ test/data/src/test.ts
@@ -3,11 +3,15 @@ import assert from 'assert';
 import exit from 'exit';
 
 import App from './lib/App.js';
+import string from './lib/string.js';
+import stringInfer from './lib/string';
 // @ts-ignore
 import stringCTS from './lib/stringCTS.cjs';
 import stringMTS from './lib/stringMTS.mjs';
 
 assert.ok(App, 'App not loaded');
+assert.equal(string, 'string', 'String not equal to string');
+assert.equal(stringInfer, 'string', 'String not equal to string');
 assert.equal(stringCTS, 'string', 'String not equal to string');
 assert.equal(stringMTS, 'string', 'String not equal to string');
 
