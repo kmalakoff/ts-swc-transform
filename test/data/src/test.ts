f1a48b1e461889c5a0d0b687eb1468d9d259e634
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-31 10:15:05 -0800

    Use typescript compiler fully

--- test/data/src/test.ts
+++ test/data/src/test.ts
@@ -5,12 +5,10 @@ import exit from 'exit';
 import App from './lib/App.js';
 // @ts-ignore
 import stringCTS from './lib/stringCTS.cjs';
-import stringMJS from './lib/stringMJS.mjs';
-import stringMTS from './lib/stringMJS.mjs';
+import stringMTS from './lib/stringMTS.mjs';
 
 assert.ok(App, 'App not loaded');
 assert.equal(stringCTS, 'string', 'String not equal to string');
-assert.equal(stringMJS, 'string', 'String not equal to string');
 assert.equal(stringMTS, 'string', 'String not equal to string');
 
 console.log('Success!');
