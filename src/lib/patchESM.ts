2e1fca4b0b73eac6dcb667adb16739311128cd37
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-31 12:00:51 -0800

    Upgrade typescript to infer extensions

---
+++ src/lib/patchESM.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import { importRegEx } from '../constants.js';
+import makeReplacements from './makeReplacements.js';
+
+const cjsExtensions = ['.cjs', '.cts'];
+const esmExtensions = ['.js', '.ts', '.tsx', '.mts', '.mjs'];
+
+export default function patchESM(output, options, basename) {
+  const cjsExtension = options.extensions?.cjs || '.cjs';
+  const esmExtension = options.extensions?.esm || '.mjs';
+
+  output.code = makeReplacements(output.code, importRegEx, esmExtensions, esmExtension);
+  output.code = makeReplacements(output.code, importRegEx, cjsExtensions, cjsExtension);
+
+  let ext = path.extname(basename);
+  if (cjsExtensions.indexOf(ext) >= 0) ext = cjsExtension;
+  else if (esmExtensions.indexOf(ext) >= 0) ext = esmExtension;
+  return ext;
+}
