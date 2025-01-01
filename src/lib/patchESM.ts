7bc7e3d8ea42f573c0e197aad86a5773635ba4cd
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-31 16:23:26 -0800

    Use default extensions for CJS and ESM in patch functions

--- src/lib/patchESM.ts
+++ src/lib/patchESM.ts
@@ -5,9 +5,12 @@ import makeReplacements from './makeReplacements';
 const cjsExtensions = ['.cjs', '.cts'];
 const esmExtensions = ['.js', '.ts', '.tsx', '.mts', '.mjs'];
 
+const DEFAULT_EXTENSION_CJS = '.cjs';
+const _DEFAULT_EXTENSION_EMS = '.mjs';
+
 export default function patchESM(output, options, basename) {
-  const cjsExtension = options.extensions?.cjs || '.cjs';
-  const esmExtension = options.extensions?.esm || '.mjs';
+  const cjsExtension = (options.extensions ? options.extensions.cjs : DEFAULT_EXTENSION_CJS) || DEFAULT_EXTENSION_CJS;
+  const esmExtension = (options.extensions ? options.extensions.esm : DEFAULT_EXTENSION_CJS) || DEFAULT_EXTENSION_CJS;
 
   output.code = makeReplacements(output.code, importRegEx, esmExtensions, esmExtension);
   output.code = makeReplacements(output.code, importRegEx, cjsExtensions, cjsExtension);
