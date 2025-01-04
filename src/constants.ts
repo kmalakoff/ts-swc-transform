b72b688517d701b5956b2de4568945c472bbcf56
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 18:43:39 -0800

    Replace SKIPS constant with hidden file check in workers

--- src/constants.ts
+++ src/constants.ts
@@ -6,5 +6,3 @@ export const moduleNameRegEx = '\\s*(?:[\\w${},\\s*]+)\\s*';
 export const requireRegEx = new RegExp(`(?:(?:var|const|let)${moduleNameRegEx}=\\s*)?require\\(${moduleDependencyRegEx}\\)?`, 'g');
 export const importRegEx = new RegExp(`${requireRegEx}|import(?:${moduleNameRegEx}from\\s*)?${moduleDependencyRegEx};?|export(?:${moduleNameRegEx}from\\s*)?${moduleDependencyRegEx}?`, 'g');
 export const extensions = ['.js', '.jsx', '.cjs', '.mjs', '.ts', '.tsx', '.cts', '.mts', '.es6', '.es'];
-
-export const SKIPS = ['.DS_Store'];
