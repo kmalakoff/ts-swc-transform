fdf5519699ffe893cb1236f6e026623d019bd8d1
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-06 20:37:08 -0800

    Upgrade modules

--- src/lib/findDependency.cjs
+++ src/lib/findDependency.cjs
@@ -3,7 +3,7 @@ const fs = require('fs');
 const resolve = require('resolve');
 
 module.exports = function findDependency(identifier, target) {
-  const packagePath = resolve.sync(identifier);
+  const packagePath = resolve.sync(`${identifier}/package.json`);
   const optionalDependencies = JSON.parse(fs.readFileSync(packagePath, 'utf8')).optionalDependencies;
   const name = Object.keys(optionalDependencies).find((name) => name.indexOf(target) >= 0);
   return name ? { name, version: optionalDependencies[name], modulePath: path.dirname(packagePath) } : null;
