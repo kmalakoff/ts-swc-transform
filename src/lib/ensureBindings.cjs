b65e621f1c7f0332dea8b1d04cc1c09e723dce59
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-06 20:37:08 -0800

    Upgrade modules

--- src/lib/ensureBindings.cjs
+++ src/lib/ensureBindings.cjs
@@ -19,12 +19,12 @@ const existsSync = (test) => {
 };
 
 const install = lazy((target) => {
-  const { name, modulePath } = findDependency('@swc/core/package.json', target);
+  const { name, modulePath } = findDependency('@swc/core', target);
   return existsSync(path.join(modulePath, '..', 'node_modules', name)) ? target : require('node-version-call')({ version, callbacks: true }, workerPath, target);
 });
 
 module.exports = resolveOnceMap((target, callback) => {
-  const { name, modulePath } = findDependency('@swc/core/package.json', target);
+  const { name, modulePath } = findDependency('@swc/core', target);
   return existsSync(path.join(modulePath, '..', 'node_modules', name)) ? callback() : installBindings(target, callback);
 });
 module.exports.sync = (target) => install(target)();
