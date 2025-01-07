dc9a8382c1172cab922942b6bdbe503b629b9e81
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-06 21:21:10 -0800

    Upgrade modules

---
+++ src/bindings/ensure.cjs
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const resolveOnce = require('resolve-once-cb');
+const findDependency = require('./findDependency.cjs');
+
+const workerPath = path.join(__dirname, 'install.cjs');
+const major = +process.versions.node.split('.')[0];
+const version = major < 14 ? 'stable' : 'local';
+
+const existsSync = (test) => {
+  try {
+    (fs.accessSync || fs.statSync)(test);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
+
+const bindings = {};
+module.exports = function ensureBindings(identifier, target, callback) {
+  if (bindings[identifier] === undefined) bindings[identifier] = {};
+  if (bindings[identifier][target] === undefined) {
+    bindings[identifier][target] = resolveOnce((cb) => {
+      const { name, modulePath } = findDependency(identifier, target);
+      return existsSync(path.join(modulePath, '..', 'node_modules', name)) ? cb() : require(workerPath)(identifier, target, cb);
+    });
+  }
+  bindings[identifier][target](callback);
+};
+
+const bindingsSync = {};
+module.exports.sync = (identifier, target) => {
+  if (bindingsSync[identifier] === undefined) bindingsSync[identifier] = {};
+  if (bindingsSync[identifier][target] === undefined) {
+    bindingsSync[identifier][target] = (() => {
+      const { name, modulePath } = findDependency(identifier, target);
+      return existsSync(path.join(modulePath, '..', 'node_modules', name)) ? target : require('node-version-call')({ version, callbacks: true }, workerPath, identifier, target);
+    })();
+  }
+};
