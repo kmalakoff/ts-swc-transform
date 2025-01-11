ac2e4b302b828e8e13895892157c9079ab1710cb
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-11 08:32:40 -0800

    Add import-meta-resolve and refactor module resolution logic

--- src/lib/patchCJS.ts
+++ src/lib/patchCJS.ts
@@ -2,7 +2,7 @@ import { extensions, requireRegEx } from '../constants';
 import makeReplacements from './makeReplacements';
 
 // https://github.com/vercel/next.js/blob/20b63e13ab2631d6043277895d373aa31a1b327c/packages/next/taskfile-swc.js#L118-L125
-const interopClientDefaultExport = "/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }";
+export const interop = "/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }";
 
 const DEFAULT_EXTENSION = '.cjs';
 
@@ -10,7 +10,7 @@ export default function patchCJS(entry, output, options) {
   const cjsExtension = (options.extensions ? options.extensions.cjs : DEFAULT_EXTENSION) || DEFAULT_EXTENSION;
 
   output.code = makeReplacements(entry, output.code, requireRegEx, extensions, cjsExtension, options);
-  output.code += interopClientDefaultExport;
+  output.code += interop;
 
   return cjsExtension;
 }
