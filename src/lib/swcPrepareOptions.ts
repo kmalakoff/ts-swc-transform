29f01492f1c3f0a7dbfcdadf1858145e31895511
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 19:20:25 -0800

    Pass cwd to installSync in swcPrepareOptions

--- src/lib/swcPrepareOptions.ts
+++ src/lib/swcPrepareOptions.ts
@@ -1,12 +1,14 @@
 import path from 'path';
+import url from 'url';
 import { installSync } from 'install-optional';
 
 import Module from 'module';
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
+const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
 import type { TsConfigResult } from 'get-tsconfig-compat';
 export default function swcPrepareOptions(tsconfig: TsConfigResult) {
-  installSync('@swc/core', `${process.platform}-${process.arch}`);
+  installSync('@swc/core', `${process.platform}-${process.arch}`, { cwd: __dirname });
   try {
     const ts = _require('typescript');
     const swc = _require('@swc/core');
