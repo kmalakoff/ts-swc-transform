2fb050fe2dcd4f2ec45c0b94a10313a6211ef658
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 19:48:43 -0700

    Upgrade modules

--- src/lib/patchCJS.ts
+++ src/lib/patchCJS.ts
@@ -2,7 +2,7 @@ import { extensions, requireRegEx } from '../constants';
 import makeReplacements from './makeReplacements';
 
 // https://github.com/vercel/next.js/blob/20b63e13ab2631d6043277895d373aa31a1b327c/packages/next/taskfile-swc.js#L118-L125
-export const interop = "/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }";
+export const interop = "//* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }";
 
 const DEFAULT_EXTENSION = '.cjs';
 
