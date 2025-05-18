042f2dde6bea1257ed45878552a0bd90cd0b1989
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 20:21:50 -0700

    Update fs-iterator and install-optional dependencies

--- assets/import-meta-resolve.cjs
+++ assets/import-meta-resolve.cjs
@@ -1478,4 +1478,4 @@ var doubleSlashRegEx = /[/\\]{2}/;
 
 exports.moduleResolve = moduleResolve;
 exports.resolve = resolve;
-//* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) { }; module.exports = exports.default; }
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) { }; module.exports = exports.default; }
