224ca6c4f56a731ee968f041c9bf5f783cc21365
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-20 20:49:10 -0700

    Upgrade modules

--- src/lib/patchCJS.ts
+++ src/lib/patchCJS.ts
@@ -3,7 +3,7 @@ import makeReplacements from './makeReplacements';
 
 // https://github.com/vercel/next.js/blob/20b63e13ab2631d6043277895d373aa31a1b327c/packages/next/taskfile-swc.js#L118-L125
 export const interop = "/* CJS INTEROP */ if (exports.__esModule && exports.default) { try { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) { exports.default[key] = exports[key]; } } catch (_) {}; module.exports = exports.default; }";
-const DEFAULT_EXTENSION = '.cjs';
+const DEFAULT_EXTENSION = '.js';
 
 import type { Output } from '@swc/core';
 import type { Entry } from 'fs-iterator';
