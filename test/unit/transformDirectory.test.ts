e3d23393177572de7bba70f9621bad4a091f90d9
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 23:05:46 -0700

    Upgrade modules

--- test/unit/transformDirectory.test.ts
+++ test/unit/transformDirectory.test.ts
@@ -36,7 +36,10 @@ function tests({ type, testFile, expectedCount, options, promise }) {
       queue.defer((cb) => {
         spawn(process.execPath, [testFile], { cwd: TMP_DIR, encoding: 'utf8' }, (err, res) => {
           if (err) console.log(err, res);
-          if (err) return done(err.message);
+          if (err) {
+            done(err.message);
+            return;
+          }
           assert.equal(cr(res.stdout).split('\n').slice(-2)[0], 'Success!');
           cb();
         });
