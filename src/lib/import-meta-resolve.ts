36e1078a45cd05110ca8560d05282922a202495a
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-28 08:27:46 -0700

    Update dev dependencies and rename import-meta-resolve assets

--- src/lib/import-meta-resolve.ts
+++ src/lib/import-meta-resolve.ts
@@ -1,7 +1,7 @@
 import Module from 'module';
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
-const resolve = _require('../../../assets/importMetaResolve.cjs').resolve;
+const resolve = _require('../../../assets/import-meta-resolve.cjs').resolve;
 
 export default function importMetaResolve(specifier: string, url: URL): string {
   return resolve(specifier, url);
