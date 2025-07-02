2da31d262002c6a74aacd5eaadfb8f80e4f56691
Kevin Malakoff <kmalakoff@gmail.com>
2025-07-02 06:46:58 -0700

    Add createMatcher and refactor file path resolution logic

--- src/resolveFileSync.ts
+++ src/resolveFileSync.ts
@@ -6,6 +6,7 @@ import toPath from './toPath.ts';
 import type { Context } from './types.ts';
 
 const indexExtensions = extensions.map((x) => `index${x}`);
+const endsWith = (string, check) => string.indexOf(check, string.length - check.length) !== -1;
 
 export default function resolveFileSync(specifier: string, context?: Context): string {
   const filePath = toPath(specifier, context);
@@ -16,7 +17,7 @@ export default function resolveFileSync(specifier: string, context?: Context): s
   try {
     if ((stat && stat.isDirectory()) || specifier[specifier.length - 1] === '/') {
       const items = fs.readdirSync(filePath);
-      const item = find(items, (x) => indexExtensions.indexOf(x) >= 0);
+      const item = find(items, (x) => endsWith(indexExtensions, x));
       if (item) return path.join(filePath, item);
     } else if (!stat && !moduleRegEx.test(specifier)) {
       const ext = path.extname(filePath);
