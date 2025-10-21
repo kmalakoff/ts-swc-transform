4c9e5979d4e30c7a31b1d5cf4816796340a14f88
Kevin Malakoff <kmalakoff@gmail.com>
2025-10-20 18:15:31 -0700

    Patch type file extensions https://github.com/microsoft/TypeScript/issues/61037

--- src/lib/patchESM.ts
+++ src/lib/patchESM.ts
@@ -2,9 +2,8 @@ import type { Output } from '@swc/core';
 import type { Entry } from 'fs-iterator';
 import path from 'path';
 import type { CompilerOptions } from 'typescript';
-import { importRegEx } from '../constants';
 import type { ConfigOptions } from '../types.ts';
-import { makeReplacements, replaceExtension } from './rewriteExtensions.ts';
+import { replaceExtension, rewriteExtensions } from './rewriteExtensions.ts';
 
 interface InternalCompilerOptions extends CompilerOptions {
   rewriteRelativeImportExtensions?: boolean;
@@ -12,9 +11,7 @@ interface InternalCompilerOptions extends CompilerOptions {
 
 export default function patchESM(entry: Entry, output: Output, options: ConfigOptions): string {
   const rewrite = ((options.tsconfig.config.compilerOptions || {}) as unknown as InternalCompilerOptions).rewriteRelativeImportExtensions;
-  if (rewrite) {
-    output.code = makeReplacements(output.code, importRegEx);
-  }
+  if (rewrite) output.code = rewriteExtensions(output.code);
 
   return replaceExtension(path.extname(entry.basename));
 }
