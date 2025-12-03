ddacd6c00b4d63ee3e77075d1b1c9b0f2b6ef1f7
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 14:45:13 -0800

    Fix typeFileRegEx regex

--- src/createMatcher.ts
+++ src/createMatcher.ts
@@ -3,5 +3,7 @@ import match from 'test-match';
 import type { Matcher, TSConfig } from './types.ts';
 
 export default function createMatcher(tsconfig: TSConfig): Matcher {
-  return match({ cwd: path.dirname(tsconfig.path), include: tsconfig.config.include as string[], exclude: tsconfig.config.exclude as string[] });
+  const include = (tsconfig.config.include || []) as string[];
+  const exclude = (tsconfig.config.exclude || []) as string[];
+  return match({ cwd: path.dirname(tsconfig.path), include, exclude });
 }
