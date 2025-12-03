08e1dd7fc3c2bbe2bfd1b4b1382eea30b84d0a51
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 14:45:13 -0800

    Fix typeFileRegEx regex

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const moduleRegEx = /^[^./]|^\.[^./]|^\.\.[^/]/;
-export const typeFileRegEx = /^[^.]+\.d\.[cm]?ts$/;
+export const typeFileRegEx = /\.d\.[cm]?ts$/;
 export const extensions = ['.js', '.jsx', '.cjs', '.mjs', '.ts', '.tsx', '.cts', '.mts', '.es6', '.es'];
